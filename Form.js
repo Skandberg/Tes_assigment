dea3c3a15359f296558f174cb0846f110877a99f
Skandberg <skandberg@accenture.com>
2019-03-16 13:24:11 +0200

    Initial Commit

---
+++ Form.js
@@ -0,0 +1,34 @@
+   $(document).on("click", "#check_text",function(){
+     
+       var field_name=$('#name');
+       var field_surname=$('#surname');
+       var field_personal_code=$('#personal_code');
+       
+       if (field_name.val()==''|| field_surname.val()==''||field_personal_code.val()=='')
+       {
+          var missing_fields='';
+          if(field_name.val()=='')
+          { 
+            missing_fields+="Name; ";
+            $("#name").css('background-color','pink');
+          }
+          if (field_surname.val()=='')
+          {
+            missing_fields+="Surname; ";
+            $("#surname").css('background-color','pink');
+          }
+          if (field_personal_code.val()=='')
+          {
+            missing_fields+="Personal code; "
+            $("#personal_code").css('background-color','pink');
+          }
+          alert("Please fill the following fields " + missing_fields);
+       }
+      else
+      {
+        alert ("Succesfully completed ");
+      }
+     });  
+     
+
+   
